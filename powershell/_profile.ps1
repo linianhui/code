d938789f9e2857439484eada35dee1de047e914a
lnh <lnhcode@outlook.com>
2017-07-29 17:43:22 +0800

    add _profile.ps1 to $Profile.

--- powershell/_profile.ps1
+++ powershell/_profile.ps1
@@ -1,5 +1,18 @@
-#########################
-# profile
-########################
+##################################
+# current user powershell profile
+#################################
+
+$InvokeSelfExpression = ". '$PSCommandPath'"
+
+# if $Profile not exist,created it. 
+if (!(Test-Path $Profile)) {
+    New-Item -Path $Profile -ItemType File -Force
+}
+
+$ProfileContent = Get-Content -Path $Profile
+if (!($ProfileContent -contains $InvokeSelfExpression)) {
+    $ProfileContent += $InvokeSelfExpression
+    Set-Content -Path $Profile -Value $ProfileContent
+}
 
 . "$PSScriptRoot/git-in-powershell.ps1"
