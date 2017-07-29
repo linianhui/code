8568fc0e4920bea6e93eda25ccef3da0851a2b54
lnh <lnhcode@outlook.com>
2017-07-29 17:58:41 +0800

    update _profile.ps1

--- powershell/_profile.ps1
+++ powershell/_profile.ps1
@@ -2,17 +2,34 @@
 # current user powershell profile
 #################################
 
-$InvokeSelfExpression = ". '$PSCommandPath'"
+# params
+Param(
+    [bool]$AutoAddProfile = $False
+)
 
-# if $Profile not exist,created it. 
-if (!(Test-Path $Profile)) {
-    New-Item -Path $Profile -ItemType File -Force
+function Add-CurrentUserPowerShellProfile () {
+    $InvokeSelfExpression = ". '$PSCommandPath'"
+
+    # if $Profile not exist,created it. 
+    if (!(Test-Path $Profile)) {
+        New-Item -Path $Profile -ItemType File -Force
+    }
+    else {
+        Write-Host "$Profile exist." -ForegroundColor Gray
+    }
+
+    $ProfileContent = Get-Content -Path $Profile
+    if (!($ProfileContent -contains $InvokeSelfExpression)) {
+        $ProfileContent += $InvokeSelfExpression
+        Set-Content -Path $Profile -Value $ProfileContent
+    }
+    else {
+        Write-Host "$InvokeSelfExpression exist." -ForegroundColor Gray
+    }
 }
 
-$ProfileContent = Get-Content -Path $Profile
-if (!($ProfileContent -contains $InvokeSelfExpression)) {
-    $ProfileContent += $InvokeSelfExpression
-    Set-Content -Path $Profile -Value $ProfileContent
+if ($AutoAddProfile) {
+    Add-CurrentUserPowerShellProfile
 }
 
 . "$PSScriptRoot/git-in-powershell.ps1"
