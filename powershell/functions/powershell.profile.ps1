3f270a797958287186a111a7b830149c042e75fa
lnh <lnhcode@outlook.com>
2017-07-30 00:06:42 +0800

    adjust powershell.profle.ps1

--- powershell/functions/powershell.profile.ps1
+++ powershell/functions/powershell.profile.ps1
@@ -3,27 +3,25 @@
 ################################
 
 function AppendScriptFile-CurrentUserPowerShellProfile ([string] $ScriptFilePath = $(throw "ScriptFilePath is null!") ) {
- 
     if (!(Test-Path $ScriptFilePath)) {
-        throw "$ScriptFilePath not found.";
+        throw "$ScriptFilePath not found."
     }
 
-    $InvokeIndexFileExpression = ". ""$ScriptFilePath"""
-
-    if (!(Test-Path $Profile)) {
+    if (Test-Path $Profile) {
+        Write-Host '$Profile already existed.' -ForegroundColor Gray
+    }
+    else {        
         New-Item -Path $Profile -ItemType File -Force
         Write-Host '$Profile created.' -ForegroundColor Green
     }
-    else {
-        Write-Host '$Profile already existed.' -ForegroundColor Gray
-    }
 
+    $InvokeScriptFileExpression = ". ""$ScriptFilePath"""
     $ProfileContent = Get-Content -Path $Profile
-    if (!($ProfileContent -contains $InvokeIndexFileExpression)) {
-        Set-Content -Path $Profile -Value "$ProfileContent`r`n$InvokeIndexFileExpression"
-        Write-Host $ScriptFilePath' append to $Profile.' -ForegroundColor Green
+    if ($ProfileContent -contains $InvokeScriptFileExpression) {
+        Write-Host $ScriptFilePath' already existed in $Profile.' -ForegroundColor Gray
     }
     else {
-        Write-Host $ScriptFilePath' already existed in $Profile.' -ForegroundColor Gray
+        Set-Content -Path $Profile -Value "$ProfileContent`r`n$InvokeScriptFileExpression"
+        Write-Host $ScriptFilePath' append to $Profile.' -ForegroundColor Green
     }
 }
