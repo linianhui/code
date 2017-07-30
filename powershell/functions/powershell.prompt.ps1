92473410a138fedbefb7e2cc39882b3806c49d0d
lnh <lnhcode@outlook.com>
2017-07-30 12:33:10 +0800

    refactor powershell prompt.

--- powershell/functions/powershell.prompt.ps1
+++ powershell/functions/powershell.prompt.ps1
@@ -6,14 +6,16 @@ function prompt () {
     # hold last exit code
     $originalLastExitCode = $LASTEXITCODE
 
-    # show git user.name and user.email 
+    # if working directory is git repository
     if (Get-GitStatus) {
+
+        # show git user.name and user.email 
         Write-Host -NoNewline "`nPS> $(Get-GitUser) :" -ForegroundColor Gray
+        
+        # show git status
+        Write-VcsStatus
     }
      
-    # show git status
-    Write-VcsStatus
-
     # show current work directory in window title
     $Host.UI.RawUI.WindowTitle = "$(Get-DisplayUserName) $(Get-Location)"
 
