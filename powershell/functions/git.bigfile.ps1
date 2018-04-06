953bec2e5396c7800af3c145aa06323a568bd485
lnh <lnhcode@outlook.com>
2018-04-06 23:07:23 +0800

    Update git.bigfile.ps1

---
+++ powershell/functions/git.bigfile.ps1
@@ -0,0 +1,40 @@
+################################
+# git find big file functions
+################################
+
+function script:Git-GetAllObjects () {
+    $gitObjects = git rev-list --objects --all | git cat-file --batch-check='%(objecttype)|%(objectname)|%(objectsize)|%(rest)'
+    return $gitObjects
+}
+
+function script:Git-ConvertToPSObject ($object) {
+    $row = $object.Split('|')
+
+    $gitObject = New-Object PSObject -Property @{
+        type = $row[0];
+        sha  = $row[1];
+        size = [int]($row[2]);
+        path = $row[3];
+    }
+  
+    return $gitObject
+}
+
+function script:Git-GetAllBlobObjects () {
+    $gitBlobObjects = @()
+    Git-GetAllObjects | Foreach-Object {
+        if ($_.StartsWith('blob')) {
+            $gitBlobObjects += (Git-ConvertToPSObject $_)
+        }
+    }
+    return $gitBlobObjects
+}
+
+
+function Git-GetBigFiles([int]$top = 20) {
+    Write-Host "find..." -ForegroundColor Green
+    Git-GetAllBlobObjects | 
+        Sort-Object size -Descending | 
+        Select-Object -First $top | 
+        Format-Table -Property sha, @{Label = "size"; Expression = {($_.size / 1MB).ToString(('0.000')) + 'MB'} }, path -Wrap
+}
