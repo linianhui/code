5cb5e1ce2a1dd4899f4bb3840113b4f809ec59c2
lnh <lnhcode@outlook.com>
2018-04-06 23:02:27 +0800

    Format ps1

--- powershell/functions/sln.ps1
+++ powershell/functions/sln.ps1
@@ -3,38 +3,38 @@
 ########################################
 
 function script:Sln-GetFiles() {
-  $currentPath = Get-Location
-  Write-Host '当前目录：' $currentPath -ForegroundColor Green
+    $currentPath = Get-Location
+    Write-Host '当前目录：' $currentPath -ForegroundColor Green
 
-  $slnFiles = Get-ChildItem -Path $currentPath -File -Filter *.sln
+    $slnFiles = Get-ChildItem -Path $currentPath -File -Filter *.sln
 
-  return $slnFiles
+    return $slnFiles
 }
 
-function script:Sln-SelectFile($slnFiles){
-  if ($slnFiles.Count -eq 0) {
-    throw '没有找到sln文件。'
-  }
+function script:Sln-SelectFile($slnFiles) {
+    if ($slnFiles.Count -eq 0) {
+        throw '没有找到sln文件。'
+    }
 
-  if ($slnFiles.Count -eq 1) {
-    return $slnFiles[0]
-  }
+    if ($slnFiles.Count -eq 1) {
+        return $slnFiles[0]
+    }
   
-  [int]$i = 0
-  $slnFiles | ForEach-Object {
-    Write-Host $i ': ' $_.Name
-    $i++
-  }
+    [int]$i = 0
+    $slnFiles | ForEach-Object {
+        Write-Host $i ': ' $_.Name
+        $i++
+    }
 
-  [int]$index = Read-Host "找到多个sln文件，请输入编号（默认0）："
-  return $slnFiles[$index]
+    [int]$index = Read-Host "找到多个sln文件，请输入编号（默认0）："
+    return $slnFiles[$index]
 }
 
 function sln() {
-  $slnFiles = Sln-GetFiles
-  $slnFile  = Sln-SelectFile $slnFiles
-  Write-Host '正在打开：' $slnFile -ForegroundColor Green
-  Invoke-Item $slnFile
+    $slnFiles = Sln-GetFiles
+    $slnFile = Sln-SelectFile $slnFiles
+    Write-Host '正在打开：' $slnFile -ForegroundColor Green
+    Invoke-Item $slnFile
 }
 
 
