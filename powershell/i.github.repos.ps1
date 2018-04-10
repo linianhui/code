08fdd1045154ed5c1922f60621e1122968bb4a24
lnh <lnhcode@outlook.com>
2018-04-10 14:06:56 +0800

    Update cdg function

--- powershell/i.github.repos.ps1
+++ powershell/i.github.repos.ps1
@@ -1,28 +1,31 @@
 ﻿# Github仓库的根目录
-$GitHubReposPath      = 'd:\.github\'
+$GitHubReposPath = 'd:\.github\'
 
 # 我的仓库的根目录
-$iGitHubReposPath    = $GitHubReposPath + '.lnh\'
+$iGitHubReposPath = $GitHubReposPath + '.lnh\'
 
 $GitHubReposPathAlias = @{
-  i    = $iGitHubReposPath;
-  iae  = $iGitHubReposPath + 'aspnetcore.example';
-  ice  = $iGitHubReposPath + 'cake.example';
-  ic9d = $iGitHubReposPath + 'channel9.downloader';
-  ices = $iGitHubReposPath + 'chrome.extensions';
-  ict  = $iGitHubReposPath + 'cnblogs.theme';
-  ic   = $iGitHubReposPath + 'code';
-  icg  = $iGitHubReposPath + 'code.guide';
-  id   = $iGitHubReposPath + 'dockerfiles';
-  igt  = $iGitHubReposPath + 'git.test';
-  ins  = $iGitHubReposPath + 'nuget.server';
-  ioe  = $iGitHubReposPath + 'oidc.example';
+    i    = $iGitHubReposPath;
+    iae  = $iGitHubReposPath + 'aspnetcore.example';
+    ice  = $iGitHubReposPath + 'cake.example';
+    ic9d = $iGitHubReposPath + 'channel9.downloader';
+    ices = $iGitHubReposPath + 'chrome.extensions';
+    ict  = $iGitHubReposPath + 'cnblogs.theme';
+    ic   = $iGitHubReposPath + 'code';
+    icg  = $iGitHubReposPath + 'code.guide';
+    id   = $iGitHubReposPath + 'dockerfiles';
+    igt  = $iGitHubReposPath + 'git.test';
+    ins  = $iGitHubReposPath + 'nuget.server';
+    ioe  = $iGitHubReposPath + 'oidc.example';
 }
 
-function cdg ([string] $path) {
-  if ($path) {
-    Set-Location $GitHubReposPathAlias[$path]
-  }else {
-    Set-Location $GitHubReposPath
-  }
+function cdg ([string] $Path) {
+    $TargetPath = $GitHubReposPath
+    
+    if ($Path) {
+        $TargetPath= $GitHubReposPathAlias[$Path]
+    }
+
+    Write-Host "cd $TargetPath" -ForegroundColor Green
+    Set-Location $TargetPath
 }
