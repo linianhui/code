ee6f01752eed8dfc3987132c595688c0807eef9a
lnh <lnhcode@outlook.com>
2018-04-10 13:59:13 +0800

    Add spring.ps1

---
+++ java/spring.ps1
@@ -0,0 +1,12 @@
+﻿################################
+# gradle functions
+################################
+Param(
+    [string]$SpringBootCliBinPath = 'd:\.java\.spring-boot-cli\bin'
+)
+
+function SpringBootCli-SetEnvironmentVariable() {
+    Env-TryAppendPathVariable -Value $SpringBootCliBinPath
+}
+
+SpringBootCli-SetEnvironmentVariable
