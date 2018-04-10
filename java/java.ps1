b2316bf40166e880c1d89c1e251f0b81baf89f7f
lnh <lnhcode@outlook.com>
2018-04-10 10:36:37 +0800

    Add java.ps1

---
+++ java/java.ps1
@@ -0,0 +1,14 @@
+﻿################################
+# java functions
+################################
+Param(
+    [string]$JavaHome = $(throw "JavaHome is null!")
+)
+
+function Java-SetEnvironmentVariable() {
+    Env-SetVariable -Variable 'JAVA_HOME' -Value $JavaHome
+    Env-SetVariable -Variable 'CLASSPATH' -Value '.;%JAVA_HOME%\lib;%JAVA_HOME%\lib\tools.jar'
+    Env-TryAppendPathVariable -Value '%JAVA_HOME%\bin'
+}
+
+Java-SetEnvironmentVariable
