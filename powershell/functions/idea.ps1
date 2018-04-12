3d3a6675e1d91bf484ac228b390bb5dffabf6b8e
lnh <lnhcode@outlook.com>
2018-04-12 10:14:09 +0800

    Add idea.ps1

---
+++ powershell/functions/idea.ps1
@@ -0,0 +1,10 @@
+﻿########################################
+# powershell open IntelliJ IDEA project
+########################################
+
+function idea() {
+    Write-Host '正在打开：idea64.exe' -ForegroundColor Green
+    idea64.exe
+}
+
+
