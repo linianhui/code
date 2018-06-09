dbddfd7214d22dbcf9657fb29ae55434078e469f
lnh <lnhcode@outlook.com>
2018-06-09 12:35:52 +0800

    Update app.tools.ps1

--- powershell/functions/env/app.tools.ps1
+++ powershell/functions/env/app.tools.ps1
@@ -18,29 +18,9 @@ function Env-SetNugetEnvironmentVariable() {
     Env-TryAppendPathVariable -Value 'd:\.app\.tools\.nuget\'
 }
 
-# https://github.com/MicrosoftArchive/redis/releases
-function Env-SetRedisEnvironmentVariable() {
-    Env-TryAppendPathVariable -Value 'd:\.app\.tools\.redis\'
-}
-
-# https://www.mongodb.org/dl/win32/x86_64-2008plus-ssl
-function Env-SetMongoDBEnvironmentVariable() {
-    Env-TryAppendPathVariable -Value 'd:\.app\.tools\.mongodb\'
-}
-
 # https://developer.android.com/studio/releases/platform-tools.html
 function Env-SetADBEnvironmentVariable() {
     Env-TryAppendPathVariable -Value 'd:\.app\.tools\.adb\'
-}
-
-# http://httpd.apache.org/download.cgi
-function Env-SetHttpdEnvironmentVariable() {
-    Env-TryAppendPathVariable -Value 'd:\.app\.tools\.httpd\'
-}
-
-# http://nginx.org/en/download.html
-function Env-SetNginxEnvironmentVariable() {
-    Env-TryAppendPathVariable -Value 'd:\.app\.tools\.nginx\'
 }
 
 # https://maven.apache.org/
---
+++ powershell/functions/env/app.web.ps1
@@ -0,0 +1,13 @@
+﻿################################
+# powershell env functions
+################################
+
+# http://httpd.apache.org/download.cgi
+function Env-SetHttpdEnvironmentVariable() {
+    Env-TryAppendPathVariable -Value 'd:\.app\.web\.httpd\'
+}
+
+# http://nginx.org/en/download.html
+function Env-SetNginxEnvironmentVariable() {
+    Env-TryAppendPathVariable -Value 'd:\.app\.web\.nginx\'
+}
