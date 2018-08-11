57062e87c1633f742e8f9d93130b3e4b03475d16
lnh <lnhcode@outlook.com>
2018-08-12 00:40:19 +0800

    Add vm.ps1

---
+++ powershell/functions/vm.ps1
@@ -0,0 +1,12 @@
+﻿################################
+# powershell vm(hyper-v) functions
+################################
+
+function VM-Start ([string] $name) {
+    Get-VM -Name $name | Start-VM
+}
+
+function VM-Stop ([string] $name) {
+    Get-VM -Name $name | Stop-VM
+}
+
