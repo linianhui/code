8ce2e443b4fd159c6971eb4dd2cd9447f4b15b53
lnh <lnhcode@outlook.com>
2018-08-12 01:01:40 +0800

    Add ssh script

--- powershell/functions/vm.ps1
+++ powershell/functions/vm.ps1
@@ -2,11 +2,38 @@
 # powershell vm(hyper-v) functions
 ################################
 
-function VM-Start ([string] $name) {
+function vm () {
+    Get-VM
+}
+
+function vm-run ([string] $name) {
     Get-VM -Name $name | Start-VM
+
+    Get-VM -Name $name
 }
 
-function VM-Stop ([string] $name) {
+function vm-stop ([string] $name) {
     Get-VM -Name $name | Stop-VM
+
+    Get-VM -Name $name
 }
 
+function ubts () {
+    run-vm-and-open-ssh -username lnh -hostname ubts
+}
+
+function ceos () {
+    run-vm-and-open-ssh -username lnh -hostname ceos
+}
+
+function run-vm-and-open-ssh (
+    [string]$username,
+    [string]$hostname,
+    [string]$port = 22) {
+
+    Write-Host "run [$hostname] hyper-v vm" -ForegroundColor Green
+    vm-run -name $hostname
+
+    Write-Host "ssh $username@$hostname -p $port" -ForegroundColor Green
+    ssh "$username@$hostname" -p $port
+}
