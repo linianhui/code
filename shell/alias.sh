535caea9a9a74d0203cc75598d1b8dcb9b4910ed
linianhui <lnhcode@outlook.com>
2018-11-12 16:13:59 +0800

    Update alias.sh

--- shell/alias.sh
+++ shell/alias.sh
@@ -24,7 +24,7 @@ alias wget='wget --continue --show-progress'
 alias sha='shasum --algorithm 1'
 alias sha256='shasum --algorithm 256'
 
-alias ping='ping -count 3'
+alias ping='ping -c 5'
 
 alias ipe='curl http://ifconfig.me/ip'
 alias ipl='ipconfig getifaddr en0'
