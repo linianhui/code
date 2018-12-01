9b229091b8f988bb4ec413cf57b12c99a1f7b9da
lnh <lnhcode@outlook.com>
2018-12-01 18:54:42 +0800

    Update .ps1

--- powershell/functions/git/.ps1
+++ powershell/functions/git/.ps1
@@ -11,3 +11,15 @@ function Git-ImportPoshGit () {
 function Git-InstallPoshGit () {
     Install-Module posh-git
 }
+
+# recurse git pull rebase
+function Git-Pull-Rebase-Recurse () {
+    Get-ChildItem -Attributes Directory -Path (Get-Location) | ForEach-Object {
+       $Path = $_.FullName
+       Write-Host "`ncd $Path" -ForegroundColor Green
+       Set-Location $Path
+       Write-Host "git pull --rebase" -ForegroundColor Green
+       git pull --rebase
+       Set-Location -Path ..
+    }
+}
