5cd12ec1cdd77a2cb590cb828fa48a914113186a
lnh <lnhcode@outlook.com>
2018-12-01 20:13:51 +0800

    Add git-warn-gc function

--- powershell/functions/git/.ps1
+++ powershell/functions/git/.ps1
@@ -15,11 +15,31 @@ function Git-InstallPoshGit () {
 # recurse git pull rebase
 function Git-Pull-Rebase-Recurse () {
     Get-ChildItem -Attributes Directory -Path (Get-Location) | ForEach-Object {
-       $Path = $_.FullName
-       Write-Host "`ncd $Path" -ForegroundColor Green
-       Set-Location $Path
-       Write-Host "git pull --rebase" -ForegroundColor Green
-       git pull --rebase
-       Set-Location -Path ..
+        $Path = $_.FullName.ToLower()
+        Write-Host "`ncd $Path" -ForegroundColor Green
+        Set-Location $Path
+        Write-Host "git pull --rebase" -ForegroundColor Green
+        git pull --rebase
+        Set-Location -Path ..
+    }
+}
+
+
+function Git-Warn-GC () {
+    Write-Host "git reflog expire --expire=now --all" -ForegroundColor Green
+    git warn-expire-reflog
+    Write-Host "git gc --prune=now --aggressive" -ForegroundColor Green
+    git warn-gc-now
+    Write-Host "git count-objects -v -H" -ForegroundColor Green
+    git size
+}
+
+function Git-Warn-GC-Recurse () {
+    Get-ChildItem -Attributes Directory -Path (Get-Location) | ForEach-Object {
+        $Path = $_.FullName.ToLower()
+        Write-Host "`ncd $Path" -ForegroundColor Green
+        Set-Location $Path
+        Git-Warn-GC
+        Set-Location -Path ..
     }
 }
--- shell/function.sh
+++ shell/function.sh
@@ -5,3 +5,12 @@ function backup_file() {
 function port(){
   lsof "-i:$1"
 }
+
+function git-warn-gc () {
+    echo -e "\033[32mgit reflog expire --expire=now --all \033[0m"
+    git warn-expire-reflog
+    echo -e "\033[32mgc --prune=now --aggressive \033[0m"
+    git warn-gc-now
+    echo -e "\033[32mcount-objects -v -H \033[0m"
+    git size
+}
