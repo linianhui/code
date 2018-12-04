92f719029cf4987110a6b90e8fb1f35a8e20f89b
lnh <lnhcode@outlook.com>
2018-12-04 21:01:31 +0800

    Update shell

--- shell/function.sh
+++ shell/function.sh
@@ -9,8 +9,8 @@ function port(){
 function git-warn-gc () {
     echo -e "\033[32mgit reflog expire --expire=now --all \033[0m"
     git warn-expire-reflog
-    echo -e "\033[32mgc --prune=now --aggressive \033[0m"
+    echo -e "\033[32mgit gc --prune=now --aggressive \033[0m"
     git warn-gc-now
-    echo -e "\033[32mcount-objects -v -H \033[0m"
+    echo -e "\033[32mgit count-objects -v -H \033[0m"
     git size
 }
