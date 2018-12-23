b8d8667272146fba6437fe48b7aa1e6ff52db57b
lnh <lnhcode@outlook.com>
2018-12-23 23:12:37 +0800

    Move docker compose and machine sh

--- shell/ubuntu/install-docker-machine.sh
+++
@@ -1,5 +0,0 @@
-curl -L https://github.com/docker/machine/releases/download/v0.15.0/docker-machine-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-machine
-
-chmod +x /usr/local/bin/docker-machine
-
-docker-machine --version
