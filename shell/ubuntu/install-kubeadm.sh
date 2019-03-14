0ef987601bf4b8f2609ede2b98e6df88dc57c8a1
lnh <lnhcode@outlook.com>
2019-03-14 21:00:04 +0800

    Add install-kubeadm.sh

---
+++ shell/ubuntu/install-kubeadm.sh
@@ -0,0 +1,13 @@
+set -x
+
+apt-get update -y
+apt-get install -y apt-transport-https
+
+curl -fsSL https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add - 
+
+cat <<-EOF > /etc/apt/sources.list.d/kubernetes.list
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+EOF  
+
+apt-get update -y
+apt-get install -y kubelet kubeadm kubectl
