aea531486ad557a8b33479d6bc47f91c161a457c
lnh <lnhcode@outlook.com>
2019-04-14 00:45:59 +0800

    Update vm.ps1

--- powershell/vm.ps1
+++ powershell/vm.ps1
@@ -11,6 +11,64 @@ function vm ([string] $name) {
     }
 }
 
+# https://docs.microsoft.com/en-us/powershell/module/hyper-v/new-vhd?view=win10-ps
+function vhd-create(
+    [string] $Path,
+    [uint64] $Size = 128GB
+) {
+    Log-Debug "New-VHD -Path $Path -Dynamic -SizeBytes $Size"
+    New-VHD `
+        -Path $Path ` 
+    -Dynamic `
+        -SizeBytes $Size
+}
+
+# https://docs.microsoft.com/en-us/powershell/module/hyper-v/new-vm?view=win10-ps
+# https://docs.microsoft.com/en-us/powershell/module/hyper-v/set-vm?view=win10-ps
+# https://docs.microsoft.com/en-us/powershell/module/hyper-v/set-vmdvddrive?view=win10-ps
+function vm-create(
+    [string] $VMName,
+    [string] $Path = 'e:\.vm',
+    [string] $ISO
+) {
+
+    Log-Debug "New-VM -Name $VMName -Path $Path -Generation 1 -SwitchName 'HVS' -NewVHDPath "$Path\$VMName\disk.vhdx" -NewVHDSizeBytes 128GB"
+    New-VM `
+        -Name $VMName `
+        -Path $Path `
+        -Generation 1 `
+        -SwitchName 'HVS' `
+        -NewVHDPath "$Path\$VMName\disk.vhdx" `
+        -NewVHDSizeBytes 128GB
+
+    Log-Debug "Set-VMProcessor -VMName $VMName -Count 2"
+    Set-VMProcessor `
+        -VMName $VMName `
+        -Count 2
+
+    Log-Debug "Set-VMMemory -VMName $VMName -DynamicMemoryEnabled $TRUE -StartupBytes 1GB -MinimumBytes 1GB -MaximumBytes 4GB"
+    Set-VMMemory `
+        -VMName $VMName `
+        -DynamicMemoryEnabled $TRUE `
+        -StartupBytes 1GB `
+        -MinimumBytes 1GB `
+        -MaximumBytes 4GB
+
+    if ($ISO) {
+        Log-Debug "Set-VMDvdDrive -VMName $VMName -Path $ISO"
+        Set-VMDvdDrive `
+            -VMName $VMName `
+            -Path $ISO
+    }
+
+    Log-Debug "Set-VM -Name $VMName -AutomaticStartAction 'Nothing' -AutomaticStopAction 'ShutDown' -CheckpointType 'Disabled'"
+    Set-VM `
+        -Name $VMName `
+        -AutomaticStartAction 'Nothing' `
+        -AutomaticStopAction 'ShutDown' `
+        -CheckpointType 'Disabled'
+}
+
 function vm-run ([string] $name) {
     Get-VM -Name $name | Start-VM
 }
