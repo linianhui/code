09dd34d464bb22096809f9e1c7052fe1c56f43b4
lnh <lnhcode@outlook.com>
2019-05-15 21:55:30 +0800

    Create ubuntu.k8s.init.sh

---
+++ shell/ubuntu.k8s.init.sh
@@ -0,0 +1,51 @@
+# wget https://raw.githubusercontent.com/linianhui/code/master/shell/ubuntu.k8s.kubeadm.init.sh
+# sudo bash ubuntu.k8s.init.sh
+
+set -x
+
+
+# 禁用防火墙
+ufw disable
+
+
+# 禁用SWAP
+# 永久禁用
+cp /etc/fstab /etc/fstab.bak
+cat /etc/fstab.bak | grep -v swap > /etc/fstab
+cat /etc/fstab
+
+
+# 禁用SELINUX
+# 永久禁用
+tee <<-EOF /etc/selinux/config
+SELINUX=permissive
+EOF
+
+
+# 修改dockerd配置
+tee <<-EOF /etc/docker/daemon.json
+{
+  "iptables": false,
+  "storage-driver": "overlay2",
+  "registry-mirrors": [
+    "https://registry.docker-cn.com"
+  ]
+}
+EOF
+systemctl daemon-reload
+systemctl restart docker
+docker info | grep -i cgroup
+
+
+# 开启IPv4转发
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+cat /etc/sysctl.conf | grep 'net.ipv4.ip_forward'
+sysctl -p
+
+
+# 配置iptables
+tee <<-EOF /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables=1
+net.bridge.bridge-nf-call-iptables=1
+EOF
+sysctl --system
