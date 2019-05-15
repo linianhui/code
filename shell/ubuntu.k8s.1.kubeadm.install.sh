98db82792b3e591ed65ff9698459c701fefebf4a
lnh <lnhcode@outlook.com>
2019-05-16 07:34:08 +0800

    Update ubuntu.k8s.1.kubeadm.install.sh

--- shell/ubuntu.k8s.1.kubeadm.install.sh
+++ shell/ubuntu.k8s.1.kubeadm.install.sh
@@ -1,14 +1,16 @@
 # wget https://raw.githubusercontent.com/linianhui/code/master/shell/ubuntu.k8s.1.kubeadm.install.sh
 # sudo bash ubuntu.k8s.1.kubeadm.install.sh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/linianhui/code/master/shell/ubuntu.k8s.1.kubeadm.install.sh)"
 
 set -x
 
 apt-get update -y
 apt-get install -y apt-transport-https
 
-curl -fsSL https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add - 
+curl -fsSL https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | \
+apt-key add - 
 
-cat <<-EOF > /etc/apt/sources.list.d/kubernetes.list
+tee <<-EOF /etc/apt/sources.list.d/kubernetes.list
 deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
 EOF
 
