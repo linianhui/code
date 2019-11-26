c0b9584980159b3bf44d330c9d50db8ecf191fb5
lnhcode <lnhcode@outlook.com>
2019-11-26 18:58:10 +0800

    Update _mac.sh

--- shell/_mac.sh
+++ shell/_mac.sh
@@ -1,7 +1,9 @@
+launchctl setenv JAVA_HOME $JAVA_HOME
 
 launchctl setenv M2_HOME $M2_HOME
 launchctl setenv MAVEN_HOME $MAVEN_HOME
 launchctl setenv MAVEN_REPOSITORY_HOME $MAVEN_REPOSITORY_HOME
+
 launchctl setenv GRADLE_HOME $GRADLE_HOME
 launchctl setenv GRADLE_USER_HOME $GRADLE_USER_HOME
 
