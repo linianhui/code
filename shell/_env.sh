cc3b6e10e866e02f28574a29ab8fa2d5dd7b021a
lnh <lnhcode@outlook.com>
2020-01-05 00:57:48 +0800

    Rename CACHE_HOME to CACHE_DIR

--- shell/_env.sh
+++ shell/_env.sh
@@ -1,10 +1,10 @@
 APP_DIR=$HOME/_app
-CACHE_HOME=$HOME/_cache
+CACHE_DIR=$HOME/_cache
 COFIG_DIR=$HOME/_config
 DATA_DIR=$HOME/_data
 
 # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
-export XDG_CACHE_HOME=$CACHE_HOME
+export XDG_CACHE_HOME=$CACHE_DIR
 export XDG_CONFIG_HOME=$COFIG_DIR
 export XDG_DATA_HOME=$DATA_DIR
 
@@ -16,13 +16,13 @@ export PATH=$PATH:$DOTNET_TOOLS_HOME
 
 # https://gradle.org/releases/ 
 export GRADLE_HOME=$APP_DIR/_gradle
-export GRADLE_USER_HOME=$CACHE_HOME/_gradle
+export GRADLE_USER_HOME=$CACHE_DIR/_gradle
 export PATH=$PATH:$GRADLE_HOME/bin
 
 # https://maven.apache.org/
 # http://maven.apache.org/configure.html
 export MAVEN_HOME=$APP_DIR/_maven
-export MAVEN_REPOSITORY_HOME=$CACHE_HOME/_maven
+export MAVEN_REPOSITORY_HOME=$CACHE_DIR/_maven
 export M2_HOME=$MAVEN_HOME
 export MAVEN_OPTS=-Dfile.encoding=UTF-8
 export MAVEN_USER_HOME=$MAVEN_HOME
@@ -40,7 +40,7 @@ export PATH=$PATH:$SPRING_BOOT_CLI_HOME/bin
 # https://docs.docker.com/machine/
 # https://docs.docker.com/machine/drivers/virtualbox/
 export DOCKER_HOME=$APP_DIR/_docker
-export MACHINE_STORAGE_PATH=$CACHE_HOME/_docker
+export MACHINE_STORAGE_PATH=$CACHE_DIR/_docker
 export VIRTUALBOX_BOOT2DOCKER_URL=$DOCKER_HOME/boot2docker.iso
 export VIRTUALBOX_UI_TYPE=headless
 export VIRTUALBOX_CPU_COUNT=1
@@ -55,7 +55,7 @@ export PATH=$PATH:$KUBECTL_HOME
 
 # https://kubernetes.io/docs/tasks/tools/install-minikube/
 export MINIKUBE_BIN=$APP_DIR/_minikube
-export MINIKUBE_HOME=$CACHE_HOME/_minikube
+export MINIKUBE_HOME=$CACHE_DIR/_minikube
 export MINIKUBE_WANTUPDATENOTIFICATION=false
 export MINIKUBE_WANTREPORTERRORPROMPT=false
 export CHANGE_MINIKUBE_NONE_USER=true
@@ -70,7 +70,7 @@ export PATH=$PATH:$HELM_BIN
 # https://github.com/mholt/caddy
 # https://caddyserver.com/docs/cli
 export CADDY_HOME=$APP_DIR/_caddy
-export CADDYPATh=$CACHE_HOME/_caddy
+export CADDYPATh=$CACHE_DIR/_caddy
 export PATH=$PATH:$CADDY_HOME
 
 # https://github.com/v2ray/v2ray-core
